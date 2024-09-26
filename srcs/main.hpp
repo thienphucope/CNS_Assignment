a23c93c9286e3597646beec1035f64233214a59c
ADMIN\HPVictus <thienphucmain1052004@gmail.com>
2024-09-26 16:12:02 +0700

    test

--- srcs/main.hpp
+++ srcs/main.hpp
@@ -1,8 +1,7 @@
 #include <iostream>
 #include <NTL/ZZ.h>
 #include <gmp.h>
+#include <vector>
 
 using namespace std;
 using namespace NTL;
-//
-//git remote set-url https://MyToken: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@github.com/thienphucope/CNS_Assignment
