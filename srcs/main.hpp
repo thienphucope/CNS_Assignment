6c927abb2156433736719a1490abc291a1d04be9
ADMIN\HPVictus <thienphucmain1052004@gmail.com>
2024-09-26 14:54:33 +0700

    token

--- srcs/main.hpp
+++ srcs/main.hpp
@@ -5,4 +5,4 @@
 using namespace std;
 using namespace NTL;
 //
-//git remote set-url https://MyToken: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@github.com/thienphucope/CNS_Assignment
+//git remote set-url https://MyToken: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@github.com/thienphucope/CNS_Assignment
