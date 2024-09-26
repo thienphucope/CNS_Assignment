8ecdadeec76e7c411a1b18d879934c0e820984ef
ADMIN\HPVictus <thienphucmain1052004@gmail.com>
2024-09-26 13:07:00 +0700

    git pass

--- srcs/main.hpp
+++ srcs/main.hpp
@@ -5,4 +5,4 @@
 using namespace std;
 using namespace NTL;
 //
-//git remote set-url https://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@github.com/thienphucope/CNS_Assignment
+//git remote set-url https://MyToken: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@github.com/thienphucope/CNS_Assignment
