17e57e17cd9ae265fdd3cc08761f75f9622a7bc2
thienphucope <thienphuc1052004@gmail.com>
2024-11-09 22:02:20 +0700

    backtocheckpoint

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,13 +1,6 @@
 #include "RSA.hpp"
 
-int main(int argc, char *argv[]) {
-        
-        if (argc != 2) {
-        cout<<"Usage: "<<argv[0]<<" <message>\n";
-        return -1;
-    }
-
-
+int main() {
         cout<<"test: ";
                 for (int i = 0; i< 10; i++){
         ZZ a = random_prime(8);
@@ -17,14 +10,13 @@ int main(int argc, char *argv[]) {
     generate_keypair(n, e, d);
 
     // Mã hóa một thông điệp
-    // Original -> ToNumber -> Encrypt -> Decrypt -> ToString -> Original
-    cout << "Original message: " << argv[1] << endl;
-    ZZ ToNumber = conv<ZZ>(stringToNumberUTF8(argv[1])); 
-    ZZ encrypted_m = encrypt(ToNumber, e, n);
-    cout << "Encrypted message: " << encrypted_m << endl;
+    ZZ m = conv<ZZ>(852025875); // Thông điệp
+    cout << "Original message: " << m << endl;
+
+    ZZ c = encrypt(m, e, n);
+    cout << "Encrypted message: " << c << endl;
 
     // Giải mã thông điệp
-    ZZ decrypted_m = decrypt(encrypted_m, d, n);
-    string ToString = numberToStringUTF8(decrypted_m);
-    cout << "Decrypted message: " << ToString << endl;
+    ZZ decrypted_m = decrypt(c, d, n);
+    cout << "Decrypted message: " << decrypted_m << endl;
 }
--- srcs/main.hpp
+++ srcs/main.hpp
@@ -1,7 +1,5 @@
 #include <iostream>
 #include <NTL/ZZ.h>
-#include <string>
-#include <stdexcept>
 
 using namespace std;
 using namespace NTL;
